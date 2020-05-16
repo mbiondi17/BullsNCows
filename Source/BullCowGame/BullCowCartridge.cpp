399e74f14bfb8b3c9751895d9f59e7aea14cd425
Matthew Biondi <mvb5bg@virginia.edu>
2020-05-16 19:09:51 -0400

    Game loop and several checks, including heterogram

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -18,45 +18,41 @@ void UBullCowCartridge::BeginPlay() // When the game starts
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
+
+    //TODO: I don't like this being on hitting enter; move it out.
+    if(bGameFinished) {
+        SetupGame();
+        return;
+    }
     
     if(Input.Compare("Introduction", ESearchCase::IgnoreCase) == 0) {
         ShowInstructions();
     }
     else {
-        //increment guess number if valid guess (decide if we should penalize for invalid guesses))
-        ++GuessesMade;
-        if(Input.Compare(HiddenWord, ESearchCase::IgnoreCase) == 0) {
-            PrintLine(TEXT("You guessed correctly! Yay!"));
-        }
-        else {
-            //TODO: Parse Bulls & Cows & Inform
-            //TODO: Check Length & Inform
-            //TODO: Check Heterogram and inform
-            //TODO: Check if all guesses expended & lose if so
-            //TODO: else prompt for next guess.
-
-            if(Input.Len() != HiddenWord.Len()) {
-                PrintLine(TEXT("The guess was %i letters long, but \nthe hidden word is %i letters long."), Input.Len(), HiddenWord.Len());
-            }
-
-            PrintLine(TEXT("You Lose, and suck balls and vagene."));
-        }
+        ProcessGuess(Input);
     }
 
-    //TODO: if the game has been completed, prompt to play again.
 
 }
 
 void UBullCowCartridge::SetupGame() {
-    MaxGuesses = 5;
     GuessesMade = 0;
+    bGameFinished = false;
     //Stretch-TODO: Get difficulty from user input
     //Stretch-TODO: Set word from appropriate difficulty list.
     //TODO: Set this from list
     HiddenWord = TEXT("Geisha");
+    HiddenWordLen = HiddenWord.Len();
+    MaxGuesses = HiddenWordLen;
     PrintLine(TEXT("You have %i attempts."), MaxGuesses);
-    PrintLine(TEXT("The Heterogram is %i letters"), HiddenWord.Len());
+    PrintLine(TEXT("The Heterogram is %i letters"), HiddenWordLen);
     PrintLine(TEXT("Press enter to submit your guess.")); 
+
+
+    const TCHAR charArray[] = TEXT("plums");
+    PrintLine(TEXT("Char 1 of the hidden word is: %c"), HiddenWord[0]);
+    PrintLine(TEXT("Char 4 of the hidden word is: %c"), HiddenWord[3]);
+
 }
 
 void UBullCowCartridge::ShowInstructions() {
@@ -64,4 +60,60 @@ void UBullCowCartridge::ShowInstructions() {
     PrintLine(TEXT("reveal correct letters in the correct spot"));
     PrintLine(TEXT("('bulls') and correct letters in the wrong"));
     PrintLine(TEXT("spot ('cows')."));
+}
+
+void UBullCowCartridge::ProcessGuess(FString Guess) {
+    if(Guess.IsEmpty()) {
+        PrintLine(TEXT("You didn't type a guess. Try again."));
+        return;
+    }
+    if(Guess.Len() != HiddenWordLen) {
+        PrintLine(TEXT("The guess was %i letters long, but the \nhidden word is %i letters long. Try Again."), Guess.Len(), HiddenWordLen);
+        return;
+    }
+    else if(!IsHeterogram(Guess)) {
+        PrintLine(TEXT("Your word was not a heterogram; \nit contained 2 or more of the same letter."));
+        return;
+    }
+
+    ++GuessesMade;
+    if(Guess.Compare(HiddenWord, ESearchCase::IgnoreCase) == 0) {
+        EndGame(true);
+    }
+    else {
+        if(GuessesMade >= MaxGuesses) {
+            EndGame(false);
+        }
+        else {
+            PrintLine(TEXT("Incorrect! You have %i guesses remaining."), MaxGuesses-GuessesMade);
+            //TODO: report bulls & cows.
+        }
+    }
+}
+
+bool UBullCowCartridge::IsHeterogram(const FString& guess) {
+    TMap<char, int32> LetterMap;
+    for(int i = 0; i < guess.Len(); i++) {
+        int32* charEntry = LetterMap.Find(guess[i]);
+        if(charEntry == nullptr) {
+            LetterMap.Add(guess[i], 1);
+        }
+        else return false;
+    }
+    return true;
+    
+}
+
+void UBullCowCartridge::EndGame(bool Victory) {
+    bGameFinished = true;
+    if(Victory) {
+        PrintLine(TEXT("Congratulations, You win!"));
+    }
+    else
+    {
+        PrintLine(TEXT("Oh no! You've exhausted all your guesses!"));
+        PrintLine(TEXT("The hidden word was %s. \nBetter luck next time."), *HiddenWord);
+    }
+    
+    PrintLine(TEXT("Press Enter to play again."));
 }
--- Source/BullCowGame/BullCowCartridge.h
+++ Source/BullCowGame/BullCowCartridge.h
@@ -14,14 +14,20 @@ class BULLCOWGAME_API UBullCowCartridge : public UCartridge
 	public:
 	virtual void BeginPlay() override;
 	virtual void OnInput(const FString& Input) override;
+	void ProcessGuess(FString Guess);
+	bool IsHeterogram(const FString& Guess);
 	void SetupGame();
 	void ShowInstructions();
+	void EndGame(bool Victory);
+
 	
 	// Your declarations go below!
 	private:
 	FString HiddenWord;
+	int32 HiddenWordLen;
 	int32 MaxGuesses;
 	int32 GuessesMade;
+	bool bGameFinished;
 
 	
 };
